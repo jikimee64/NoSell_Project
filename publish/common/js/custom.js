7e2fe449de5437e62667f55e4686b1d3f287ea75
threekate125 <66197547+threekate125@users.noreply.github.com>
2021-02-09 21:47:29 +0900

    html_tag_수정

--- publish/common/js/custom.js
+++ publish/common/js/custom.js
@@ -79,3 +79,21 @@ $(document).ready(function(){
         return false;
     });
 });
+
+// include
+window.addEventListener('load', function() {
+    var allElements = document.getElementsByTagName('*');
+    Array.prototype.forEach.call(allElements, function(el) {
+        var includePath = el.dataset.includePath;
+        if (includePath) {
+            var xhttp = new XMLHttpRequest();
+            xhttp.onreadystatechange = function () {
+                if (this.readyState == 4 && this.status == 200) {
+                    el.innerHTML = this.responseText;
+                }
+            };
+            xhttp.open('GET', includePath, true);
+            xhttp.send();
+        }
+    });
+});
